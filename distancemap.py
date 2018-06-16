8acc6832397bfd2b6290799c420bc6d08f3e13e1
Peleg <peleg@example.com>
2018-06-16 11:04:55 +0000

    3-connected dilute

--- distancemap.py
+++ distancemap.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-from functools import reduce
+from functools import reduce, partial
 from itertools import repeat, product
 from operator import or_, add
 from random import randint
@@ -61,18 +61,18 @@ def adjesent_m(pixel, mask):
     def is_in(pixel):
         r, c = pixel
         return is_in_image((r, c), mask.shape) and mask[r, c]
-    
+
     def add_offset(offset):
         return tuple(map(add, pixel, offset))
-    
+
     neighbors_4 = [offset
                    for offset in [(1, 0), (0, 1), (-1, 0), (0, -1)]
                    if is_in(add_offset(offset))]
-    
+
     neighbors_diag = [(o_r, o_c)
                       for o_r, o_c in [(1, 1), (-1, 1), (-1, -1), (1, -1)]
                       if set([(0, o_r), (o_c, 0)]).isdisjoint(neighbors_4) and is_in(add_offset((o_r, o_c)))]
-    
+
     return [add_offset(offset) for offset in neighbors_4 + neighbors_diag]
 
 
@@ -96,8 +96,11 @@ def arrayuint8(rows):
 def imshow_gray(image, figsize=(50, 50)):
     figure(figsize=figsize)
     imshow(image, cmap='gray')
-    
+
 def as_display(image):
+    if image.shape[-1] == 3:
+        return image
+
     return cv2.cvtColor(cv2.normalize(image.astype(np.float),
                                       None,
                                       0,
@@ -108,11 +111,11 @@ def as_display(image):
 def rand_color():
     return (randint(150, 200), randint(150, 200), randint(0, 255))
 
-def show_lines(image, lines, colors):
+def show_lines(image, lines, colors, width=1):
     image_with_lines = as_display(image)
     for ((point1, point2), color) in zip(lines, colors):
-        cv2.line(image_with_lines, point1, point2, color, 1)
-        
+        cv2.line(image_with_lines, point1, point2, color, width)
+
     return image_with_lines
 
 def show_points(image, points, radius=2):
@@ -205,15 +208,17 @@ imshow_gray(LOCAL_DIST_MAXIMA, figsize=(50, 50))
 # 
 # We extract the vertices and the edges from the local maxima matrix
 
-# ## Neighbor counting
-# 1. For a pixel $p$ it is defined it has a neighbors in a direction $d$ , if it has non-zero pixels in that direction.
-# 2. We count of number of directions a pixel has neighbors on. we get a number in $0\dots4$.
+# ## Junction pixels
 # 
-#    e.g, a pixel with one neighbor (left) and a pixel with two neighbors(right):
+# Using erode, we find juntions checking various predefined shapes.
 # 
-#    $\begin{pmatrix}0&0&0&0&0\\0&0&0&0&0\\0&0&\textbf{p}&0&0\\0&0&1&0&0\\0&0&1&0&0 \end{pmatrix}$
-#    $\qquad$
-#    $\begin{pmatrix}0&0&0&0&0\\0&0&0&0&0\\0&0&\textbf{p}&1&1\\0&0&1&0&0\\0&0&1&0&0 \end{pmatrix}$
+# e.g
+# 
+# Given the folowwing shape,
+# 
+# $\begin{pmatrix}0&0&0&0&0\\0&0&0&0&0\\1&1&\textbf{p}&1&1\\0&0&1&0&0\\0&0&1&0&0 \end{pmatrix}$
+# 
+# If the folowwing shape exists in local maxima (meaning all if the shape pixels are $1$), the pixel $p$ is a junciton pixel.
 
 # In[10]:
 
@@ -221,7 +226,7 @@ imshow_gray(LOCAL_DIST_MAXIMA, figsize=(50, 50))
 def rotations(mat):
     return (mat, mat.T, flip(mat, 0), flip(mat.T, 1))
 
-def vertice_candidates(binary):
+def mark_junction_pixels(binary):
     junctions = map(arrayuint8, [
         [
             [0, 0, 0, 0, 0],
@@ -229,6 +234,28 @@ def vertice_candidates(binary):
             [0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0],
             [1, 1, 1, 1, 1],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ],
+        [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0],
+        ],
+        [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 1, 0],
+            [1, 1, 1, 0, 0],
             [0, 0, 1, 0, 0],
             [0, 0, 1, 0, 0],
             [0, 0, 1, 0, 0],
@@ -250,7 +277,7 @@ def vertice_candidates(binary):
             [0, 0, 0, 0, 0],
             [1, 0, 0, 0, 1],
             [0, 1, 0, 1, 0],
-            [0, 0, 1, 1, 0],
+            [0, 0, 1, 0, 0],
             [0, 0, 1, 0, 0],
             [0, 0, 1, 0, 0],
             [0, 0, 1, 0, 0],
@@ -278,27 +305,26 @@ def vertice_candidates(binary):
 
     return reduce(or_, map(filter_junction, rotated_mats), zeros_like(binary))
 
-imshow(LOCAL_DIST_MAXIMA + vertice_candidates(LOCAL_DIST_MAXIMA),
+imshow(LOCAL_DIST_MAXIMA + mark_junction_pixels(LOCAL_DIST_MAXIMA),
        figure=figure(figsize=(50, 50)))
 
 
-# ## Centeroids
-# 1. Let $FM$ be pixels $p$ with [neighbor count](#Neighbor-counting) $n(p) > 2$ 
-# 2. Graph vertices are defined as the centeroids of connected components of $FM$ 
+# ## Vertices
+# We define 
 
 # In[11]:
 
 
-def vertices(skeleton):
-    vertex_pixels = vertice_candidates(skeleton)
-    centeroids = cv2.connectedComponentsWithStats(
-        vertex_pixels,
+def extract_vertices(junction_pixels):
+    _, labels, _, centeroids = cv2.connectedComponentsWithStats(
+        junction_pixels,
         connectivity=4
-    )[-1]
-    return [tuple(map(int, point)) for point in centeroids[1:-1]]
+    )
+    return ([tuple(map(int, point)) for point in centeroids[1:-1]],
+            labels - 1)
 
 
-LOCAL_MAX_VERTS = vertices(LOCAL_DIST_MAXIMA)
+LOCAL_MAX_VERTS, LABELS = extract_vertices(mark_junction_pixels(LOCAL_DIST_MAXIMA))
 imshow(show_points(LOCAL_DIST_MAXIMA, LOCAL_MAX_VERTS, 3),
        figure=figure(figsize=(50, 50)))
 
@@ -306,12 +332,11 @@ imshow(show_points(LOCAL_DIST_MAXIMA, LOCAL_MAX_VERTS, 3),
 #  # Edges
 
 # ## BFS
-# * We define set $C=$ [centeroids](#Centeroids)
-# * While $C\neq \emptyset$
-#   - Start from some $c \in C$ and set $C = C - \{c\}$
-#   - Perform a bfs scan on [local maxima](#Local-maxima)
-#   - Define current graph vertice as closest vertice at the curr pixels $p$ rectangle enviroment of some $r$
-#   - If found vertice is different than pervious found vertice in scan, we add an edge connecting curr vertice to found one
+# * We define set $V=$ [vertices](#vertices)
+# * While $V\neq \emptyset$
+#   - Start from some $v \in V$ and set $V = V - \{v\}$
+#   - Perform a $BFS$ scan on [local maxima](#Local-maxima) starting from $v$ iterating m-adjecent neighbors
+#   - Add all found neighbors of $v$ into the graph as vonnected to $v$
 
 # In[12]:
 
@@ -322,68 +347,119 @@ def area_to_vert(verts, radius):
                 for point in product(range(x - radius, x + radius),
                                      range(y - radius, y + radius)))
 
+def pixel_vert(pos, verts, labels_map, area_map):
+    row, col = pos
+    label = labels_map[row, col]
+    if 0 <=  label < len(verts):
+        return verts[label]
+    else:
+        return area_map.get(pos)
 
-# In[13]:
+def mask_connected(start, search_mask, covermap):
+    visited = set()
+    bfs_q = deque([start])
 
+    while bfs_q:
+        pos = bfs_q.popleft()
+        vert = covermap(pos) or start
+        if start == vert:
+            nextvs = [v for v in adjesent_m(pos, search_mask) if v not in visited]
+            visited.update(nextvs)
+            bfs_q.extend(nextvs)
+        else:
+            yield vert
 
-def build_graph(search_mask, covermap, verts):
-    unvisited_vertices = verts.copy()
+def build_graph(verts, find_connected_verts):
+    edges = ((v1, v2)
+             for v1 in verts
+             for v2 in find_connected_verts(v1))
 
     graph = defaultdict(set)
 
-    while unvisited_vertices:
-        unvisited = search_mask.copy()
-        start = unvisited_vertices.pop()
-        bfs_q = deque([start])
-        while bfs_q:
-            pos = bfs_q.popleft()
-            vert = covermap.get(pos, start)
+    for v1, v2 in edges:
+        graph[v1].add(v2)
+        graph[v2].add(v1)
 
-            if start == vert:
-                nextvs = adjesent_m(pos, unvisited)
-                for r, c in nextvs:
-                    unvisited[r, c] = 0
-                bfs_q.extend(nextvs)
-            else:
-                graph[start].add(vert)
-                graph[vert].add(start)
     return graph
 
+def remove_verts(graph, rmverts):
+    for vert in rmverts:
+        graph.pop(vert)
 
-def edges(graph):
+    graph.update((vert, neighbors - rmverts)
+                 for vert, neighbors in graph.items())
+
+def graph_edges(graph):
     return set(tuple(sorted(((c1, r1), (c2, r2))))
                for (r1, c1), connecetd in graph.items()
                for (r2, c2) in connecetd)
 
-ROW_INDEX_VERTS = [(c, r) for r, c in LOCAL_MAX_VERTS]
-GRAPH = build_graph(
-    search_mask=LOCAL_DIST_MAXIMA,
-    covermap=area_to_vert(ROW_INDEX_VERTS, 4),
-    verts=set(ROW_INDEX_VERTS))
 
-imshow(show_lines(TEXT_SHOW, edges(GRAPH), repeat((150, 150, 0))),
+def graph_vertices(graph):
+    return graph.keys()
+
+ROW_INDEX_VERTS = [(c, r) for r, c in LOCAL_MAX_VERTS]
+
+GRAPH = build_graph(
+    verts=set(ROW_INDEX_VERTS),
+    find_connected_verts=partial(mask_connected,
+                                 search_mask=LOCAL_DIST_MAXIMA,
+                                 covermap=partial(
+                                     pixel_vert,
+                                     labels_map=LABELS,
+                                     verts=ROW_INDEX_VERTS,
+                                     area_map=area_to_vert(ROW_INDEX_VERTS, 4)
+                                 )))
+
+imshow(
+    show_points(
+        show_lines(
+            LOCAL_DIST_MAXIMA,
+            graph_edges(GRAPH),
+            repeat((200, 24, 0)),
+            width=1),
+        LOCAL_MAX_VERTS, 2),
+    figure=figure(figsize=(50, 50)))
+
+imshow(show_lines(TEXT_SHOW, graph_edges(GRAPH), repeat((150, 150, 0))),
        figure=figure(figsize=(50, 50)))
 
 
-# # Remove leafs
+# # Dilute graph to 3-connected
+# 
+# Until we get a graph where all it's vertices have at least 3 neighbors, we rebuild the graph by find all 3 connected neighbors for each 3-connected vertice
 
-# In[14]:
+# In[13]:
 
 
-def clear_leafs(graph):
+def graph_connected(start, graph):
+    visited = set()
+    bfs_q = deque([start])
+
+    while bfs_q:
+        vs = [(v, len(graph[v])) for v in graph[bfs_q.popleft()]
+              if v not in visited and v != start]
+        nextvs = [v for v, n_vs in vs if n_vs <= 2]
+        visited.update(nextvs)
+        bfs_q.extend(nextvs)
+
+        for v in [v for v, n_vs in vs if n_vs > 2]:
+            yield v
+
+def build_3_connected(graph):
+    graph3 = graph.copy()
     while True:
-        leafs = set([v for v, connected in graph.items() if len(connected) <= 1])
+        verts3 = [v for v, n_vs in graph3.items() if len(n_vs) > 2]
 
-        if not leafs:
-            return
+        if len(verts3) == len(graph3.keys()):
+            return graph3
 
-        for leaf in leafs:
-            graph.pop(leaf)
+        graph3 = build_graph(
+            verts=verts3,
+            find_connected_verts=partial(graph_connected, graph=graph3))
 
-        graph.update((vert, neighbors - leafs)
-                     for vert, neighbors in graph.items())
+GRAPH_3 = build_3_connected(GRAPH)
 
-clear_leafs(GRAPH)
-imshow(show_lines(TEXT_SHOW, edges(GRAPH), repeat((100, 150, 0))),
+imshow(show_lines(TEXT_SHOW, graph_edges(GRAPH_3), repeat((100, 50, 150))),
        figure=figure(figsize=(50, 50)))
 
